8473b61b09e8d1ef102c4663436c5cc9df9bf5ce
Javi Ĺopez Teran <alum.jlopezt.2020@iesalixar.org>
2020-11-10 13:46:49 +0100

    Initial commit

---
+++ src/com/jlopezt/Main.java
@@ -0,0 +1,9 @@
+package com.jlopezt;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println( "Hola crack!" );
+        System.out.println("segunda línea");
+    }
+}
---
+++ src/com/jlopezt/Secundario.java
@@ -0,0 +1,4 @@
+package com.jlopezt;
+
+public class Secundario {
+}
